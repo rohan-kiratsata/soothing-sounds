866c36a3f377aee7ccc1b798959cb05494f6dacc
Rohan Kiratsata <kiratsatarohan1234@gmail.com>
2021-11-23 18:02:51 +0530

    Site Updated

--- js/main.js
+++ js/main.js
@@ -29,6 +29,12 @@ var rain = document.getElementById("rain-aud");
 var thunder = document.getElementById("thunder-aud");
 var wind = document.getElementById("wind-aud");
 var seagull = document.getElementById("seagull-aud");
+var crickets = document.getElementById("cricket-aud");
+var coffee = document.getElementById("coffee-aud");
+var campfire = document.getElementById("fire-aud");
+var keyboard = document.getElementById("keyboard-aud");
+var waterfall = document.getElementById("waterfall-aud");
+var meditate = document.getElementById("meditate-aud");
 
 function play(audioElem){
     audio = audioElem;
@@ -43,6 +49,7 @@ function play(audioElem){
 }
 
 function changeVol(volumeAmount, audioElem){
+    
     audObj = audioElem;
     audObj.volume = volumeAmount;
 }
