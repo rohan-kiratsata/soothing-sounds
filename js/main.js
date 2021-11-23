e085b4a89014cb50169dde92c2960d14ad098147
Rohan Kiratsata <kiratsatarohan1234@gmail.com>
2021-11-23 16:30:11 +0530

    Audio added

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// var ocean = new Audio('audio/oceanWave.mp3')
+
+
+// var isPlaying = false;
+
+// function play(audioFile){
+//     var audio = audioFile;
+//     audio.play();
+// }
+
+// function stop(audioFile){
+//     var stopAudio = audioFile;
+//     stopAudio.stop();
+//     isPlaying = false;
+// }
+
+// function togglePlay(audioFile){
+//     audio = audioFile;
+//     if(isPlaying == false){
+//         function play(audio);
+//     }
+//     else{
+//         function stop(audio);
+//     }
+// }
+var ocean = document.getElementById("ocean-aud");
+var bird = document.getElementById("bird-aud");
+var rain = document.getElementById("rain-aud");
+var thunder = document.getElementById("thunder-aud");
+var wind = document.getElementById("wind-aud");
+var seagull = document.getElementById("seagull-aud");
+
+function play(audioElem){
+    audio = audioElem;
+    audio.loop = true;
+
+    if(audio.paused){
+        audio.play();
+    }
+    else{
+        audio.pause();
+    }
+}
+
+function changeVol(volumeAmount, audioElem){
+    audObj = audioElem;
+    audObj.volume = volumeAmount;
+}
